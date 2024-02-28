38f385a340c45c5701e7ec7cff7b83b27c26e6c6
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-02-28 17:24:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+function generalexcuse() {
+  let who = [
+    "The Dog",
+    "My Neighbor",
+    "The Police",
+    "Your Teacher",
+    "His Parents"
+  ];
+  let action = ["ate", "throw away", "push", "vomit", "robbed"];
+  let what = ["me", "my homework", "my Nintendo", "my car", "my phone"];
+  let when = [
+    "yesterday",
+    "while i was sleeping",
+    "after work",
+    "last month",
+    "when i finished"
+  ];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindex] +
+    " " +
+    action[actionindex] +
+    " " +
+    what[whatindex] +
+    " " +
+    when[whenindex]
+  );
+}
+
+window.onload = () => {
+  //write your code here()
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generalexcuse();
+  });
+
   console.log("Hello Rigo from the console!");
 };
